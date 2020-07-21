9a29f7231fb719b0ba36fe8ad5d2128a1eb202ca
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-07-21 10:52:53 +0200

    Update main.cpp

--- SetCombination/Sources/main.cpp
+++ SetCombination/Sources/main.cpp
@@ -1,91 +1,21 @@
-#ifndef SET_HPP_INCLUDED
-#define SET_HPP_INCLUDED
+#include "combination.hpp"
 
-#include <iostream.h>
+int main()
+{
+    Set<int> s1, s2, s3(s1.getBr(),s1), s4(s2.getBr(),s2);
 
-template <class T>
-class Set{
-    protected:
-        T skup[20];
-        int brEl;
-    public:
-        Set() { brEl=0; }
-        Set(int, const Set<T>&);
-        int getBr() const { return brEl; }
-        ~Set() {}
-        Set<T>& operator=(const Set<T>&);
-        Set<T>& operator+=(const Set<T>&);
-        Set<T>& operator*=(const Set<T>&);
-        void insert(const T&);
-        void remove(int k);
-        void print();
-};
+    s1.insert(10);
+    s1.insert(3);
+    s1.print();
 
-template <class T>
-Set<T>::Set(int n, const Set<T> &a){
-    brEl=n;
-    for (int i=0; i<n; i++)
-        skup[i]=a.skup[i];
+
+    s2.insert(32);
+    s2.insert(1);
+    s2.insert(10);
+    s2.print();
+
+    std::cout << s3.getBr() << std::endl;
+    s3.print();
+
+    return 0;
 }
-
-template <class T>
-Set<T>& Set<T>::operator=(const Set<T> &s){
-    brEl=s.brEl;
-    for (int i=0; i<20; i++)
-        skup[i]=0;
-    for (int i=0; i<s.brEl; i++)
-        skup[i]=s.skup[i];
-
-    return *this;
-}
-
-template <class T>
-Set<T>& Set<T>::operator+=(const Set<T> &s){
-    bool isHere;
-    cout<<brEl<<"    "<<s.brEl<<endl;
-    for (int i=0; i<s.brEl; i++){
-        isHere=false;
-        for (int j=0; j<brEl; j++){
-            cout<<i<<"-"<<s.skup[i]<<" "<<skup[j]<<endl;
-            if (skup[j]==s.skup[i]) isHere=true;}
-        if (!isHere) insert(s.skup[i]);
-    }
-    return *this;
-}
-
-template <class T>
-Set<T>& Set<T>::operator*=(const Set<T> &s){
-    bool isHere;
-    for (int i=0; i<brEl; i++){
-        isHere=false;
-        for (int j=0; j<s.brEl; j++){
-            cout<<skup[j]<<" "<<s.skup[i]<<endl;
-            if (skup[j]==s.skup[i]) isHere=true;}
-        if (!isHere) remove(i);
-    }
-    return *this;
-}
-
-template <class T>
-void Set<T>::insert(const T &vr){
-    skup[brEl]=vr;
-    brEl++;
-}
-
-template <class T>
-void Set<T>::remove(int k){
-    skup[k-1]=0;
-    for (int i=k; i<brEl; i++)
-        skup[i]=skup[i+1];
-    brEl--;
-};
-
-template <class T>
-void Set<T>::print(){
-    cout<<"-------SKUP-------brEl="<<brEl<<"---"<<endl;
-    for (int i=0; i<brEl; i++)
-        cout<<i<<". "<<skup[i]<<endl;
-    cout<<"---------------------------"<<endl;
-}
-
-#endif // SET_HPP_INCLUDED
