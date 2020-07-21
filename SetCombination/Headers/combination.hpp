69b899a5378f88070fddde1f8ffcb9cfdf518b92
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-07-21 10:58:48 +0200

    Create combination.hpp

---
+++ SetCombination/Headers/combination.hpp
@@ -0,0 +1,19 @@
+#ifndef KOMBINACIJA_HPP_INCLUDED
+#define KOMBINACIJA_HPP_INCLUDED
+
+#include "set.hpp"
+
+class Kombinacija{
+    private:
+        Set<int> brojevi;
+    public:
+        Kombinacija(const Set<int> &a) { brojevi=a; }
+        Kombinacija(const Kombinacija &k) { brojevi=k.brojevi; }
+        ~Kombinacija() {}
+        void dodaj(int broj) { brojevi.insert(broj); }
+        void ukloni(int i) { brojevi.remove(i); }
+        void print() { brojevi.print(); }
+        void spoji(const Kombinacija &k) {
+            brojevi += k.brojevi;
+        }
+};
